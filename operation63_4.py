274a9cfedc147539f72c193a396b8a29c0c01872
SOORYASIVAGANESAN <147529737+SOORYASIVAGANESAN@users.noreply.github.com>
2024-06-26 15:39:17 +0530

    Beta_Code.py

--- operation63_4.py
+++ operation63_4.py
@@ -119,4 +119,52 @@ for j in range(i,100):
             i=j
             break
 
+options = webdriver.ChromeOptions()
+options.add_argument('--user-data-dir=C:\\Users\\Hp\\AppData\\Local\\Google\\Chrome\\User Data')
+options.add_experimental_option("detach", True)
+options.add_argument('--profile-directory=Profile 2')
+
+url= "https://www.instagram.com/direct/inbox/"
+master_username= "if_i_were_true_abt_u"
+password= "$Pq12345678"
+#########################################################################################
+       
+driver = webdriver.Chrome(executable_path="E:\\c data\\Desktop\\operation63\\chromedriver.exe",chrome_options=options)
+driver.get(url)
+time.sleep(5)
+
+
+#clicking on "log in" button
+Sumbit= driver.find_element(By.XPATH,"//*[contains(text(), 'Log In')]")
+Sumbit.click()
+
+
+#entering user_id
+user_name=WebDriverWait(driver, 10).until(
+        expected_conditions.presence_of_element_located((By.NAME, 'username')))
+user_name.send_keys(master_username)
+
+
+#entering password
+user_pass=WebDriverWait(driver, 10).until(
+        expected_conditions.presence_of_element_located((By.NAME, 'password')))
+user_pass.send_keys(password)
+
+
+#clicking on "log in" button
+Sumbit= driver.find_element(By.XPATH,"//*[contains(text(), 'Log in')]")
+Sumbit.click()
+time.sleep(5)
+
+#removing first two pop-ups (restore and notifications)
+if driver.find_element(By.CLASS_NAME,'_ac8f'):
+    driver.find_element(By.CLASS_NAME,'_ac8f').click()
+
+if driver.find_element(By.CLASS_NAME,'_a9-z'):
+    driver.find_element(By.CLASS_NAME,'_a9-z').click()
+time.sleep(5)
+
+#clicking message button
+driver.find_element(By.XPATH,"//*[contains(text(), 'Message')]").click()
+
     
